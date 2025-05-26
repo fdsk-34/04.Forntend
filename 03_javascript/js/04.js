6ad4b406b0dcc4e3bb1d487406559060ee15876d
Leejungmin <djfjfj2@naver.com>
2025-05-26 11:02:42 +0900

    자바스크립트트

---
+++ 03_javascript/js/04.js
@@ -0,0 +1,7 @@
+ function over(obj){     //over  함수  : 매개 변수로 obj
+            obj.src = './img/banana.png';
+
+        }
+        function out(obj){      //out  함수수
+            obj.src='./img/apple.png';
+        }
